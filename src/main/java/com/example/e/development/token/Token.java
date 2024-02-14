431bf303bd43587b5a195e5475fba8077ba632bb
Scout254 <mutinda947@gmail.com>
2024-02-15 01:23:56 +0300

    token class implemented

--- src/main/java/com/example/e/development/token/Token.java
+++ src/main/java/com/example/e/development/token/Token.java
@@ -1,4 +1,30 @@
 package com.example.e.development.token;
 
+
+import com.example.e.development.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Token {
+    @Id
+    @GeneratedValue
+    public Long id;
+    @Column(unique = true)
+    public String token;
+    @Enumerated(EnumType.STRING)
+    public TokenType tokenType = TokenType.BEARER;
+    public boolean revoked;
+    public boolean expired;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public User user;
+
 }
--- src/main/java/com/example/e/development/token/TokenType.java
+++ src/main/java/com/example/e/development/token/TokenType.java
@@ -1,4 +1,5 @@
 package com.example.e.development.token;
 
 public enum TokenType {
+    BEARER
 }

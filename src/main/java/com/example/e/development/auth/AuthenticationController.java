eda8f57663ab882f711150777450a83e55531efe
Scout254 <mutinda947@gmail.com>
2024-02-15 01:19:24 +0300

    authentication impelementation:auth controller

---
+++ src/main/java/com/example/e/development/auth/AuthenticationController.java
@@ -0,0 +1,37 @@
+package com.example.e.development.auth;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+
+public class AuthenticationController {
+    private final AuthenticationService service;
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(service.register(request));
+    }
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        System.out.println("Received email: " + request.getEmail());
+        System.out.println("Received password: " + request.getPassword());
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        service.refreshToken(request, response);
+    }
+
+}
---
+++ src/main/java/com/example/e/development/auth/RegisterRequest.java
@@ -0,0 +1,4 @@
+package com.example.e.development.auth;
+
+public class RegisterRequest {
+}
